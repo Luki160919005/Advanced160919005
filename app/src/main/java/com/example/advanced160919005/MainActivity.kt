7fa24cea1027eeca1d0bd6c3afdf353c3ccf1b07
1luki <1lukitaknow@gmail.com>
2022-02-08 21:45:01 +0700

    Init

---
+++ app/src/main/java/com/example/advanced160919005/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.advanced160919005
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // a new comment
+        // we need to implement login features
+    }
+}
